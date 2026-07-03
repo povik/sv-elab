2333d19e73efc549226017dce242a8184a72b0d3
Miodrag Milanovic <mmicko@gmail.com>
2026-07-03 16:11:24 +0200

    Fix old yosys crash

--- src/slang_frontend.cc
+++ src/slang_frontend.cc
@@ -3674,7 +3674,7 @@ struct SlangFrontend : Frontend {
 		(void) filename;
 		log_header(design, "Executing SLANG frontend.\n");
 
-		auto guard = slang::OS::captureOutput([&](std::string_view text, bool) { log("%s", text); });
+		auto guard = slang::OS::captureOutput([&](std::string_view text, bool) { log("%s", std::string(text).c_str()); });
 		// names of RTLIL modules added in this invocation; does not include blackboxes
 		std::vector<RTLIL::IdString> emitted_module_names;
 		slang::driver::Driver driver;
@@ -3950,7 +3950,7 @@ struct TestSlangExprPass : Pass {
 	{
 		log_header(d, "Executing TEST_SLANGEXPR pass.\n");
 
-		auto guard = slang::OS::captureOutput([&](std::string_view text, bool) { log("%s", text); });
+		auto guard = slang::OS::captureOutput([&](std::string_view text, bool) { log("%s", std::string(text).c_str()); });
 		slang::driver::Driver driver;
 		driver.addStandardArgs();
 		SynthesisSettings settings;
